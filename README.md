0dbbfd496b0d11d0bb3fada7967f8b20b14ccbfa
dl011492 <dl011492@gmail.com>
2025-01-02 10:51:10 +0100

    New version 08 with aclImdb zip file

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Deep Learning Dashboard
 
 **Deep Learning Dashboard** is a Python and TensorFlow project that allows one to select between standard machine learning
-datasets like: MNIST, Fashion MNIST, Kaggle cats and dogs and Oxford-IIIT pets.  
-It is designed to easily display samples of selected datasets, select a specific neural network model (MLP 512, CNN Small,
-CNN Medium with data augmentation, VGG16-DA, mini Xception, Segmentation, sentiment analysis), monitor their training and
+datasets like: MNIST, Fashion MNIST, Kaggle cats and dogs, Oxford-IIIT pets, imdb and aclImdb.  
+It is designed to easily display samples of selected datasets, select a specific neural network model (for example: MLP 512,
+CNN Medium with data augmentation, mini Xception, Segmentation, Transformer Encoderetc), monitor their training and
 evaluate the results.  
 The scripts are by no means complete. Feel free to try to finish them, implement new models and improve them.  
-**Warning** The datasets in this repository are compressed, but still large (1.2 GB). Be aware of this when cloning or
+**Warning** The datasets in this repository are compressed, but still large (2.6 GB). Be aware of this when cloning or
 downloading files.
 
 ![Deep learning dashboard example 1](https://github.com/dl011492/Deep-Learning-Dashboard/blob/main/figures/dld_v02.png)
@@ -18,19 +18,24 @@ MNIST and Fashion MNIST are already part of TensorFlow Keras and one should not 
 [Kaggle](https://www.kaggle.com/datasets/tongpython/cat-and-dog) is not the original dataset but a smaller dataset organized
 and made smaller according to the example described in Chollet's book, p. 212.  
 For the [Oxford-IIIT pets](https://www.robots.ox.ac.uk/~vgg/data/pets/) dataset, I used the book's example on p. 240. A minor speedup
-in loading and pre-processing is achieved by accessing the provided ./data/pets/oxford-IIIT_pets.npz file directly.
+in loading and pre-processing is achieved by accessing the provided ./data/pets/oxford-IIIT_pets.npz file directly.  
+For text models, I am using the data from A. Maas [aclImdb](https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz) from the
+book's example on p.  320.
 
 ---
 ## Requirements
 This simple GUI is created using Tkinter. It works fine with the following minimum requirements: 
-- Python 3.9
+- Python >= 3.9
 - TensorFlow 2.10 (to be able to use [CUDA 11.2](https://developer.nvidia.com/cuda-11.2.0-download-archive)
   [cuDNN 8.1](https://developer.nvidia.com/rdp/cudnn-archive) on Windows 10)
-- The script should also work on newer TensorFlow versions under Linux
-- At least 1.1 GB of free disk space and 16 GB of RAM
+- The script should also work on newer TensorFlow versions (e.g. 2.18) under Linux
+- At least 3.0 GB of free disk space and 16 GB of RAM
 
 ## Features
-
+All the models are taken from F. Chollet's book (see Credits).
+- **Models for computer vision**: MLP 512, MLP-2L-96, MLP-128-Dropout, CNN Small for the datasets MNIST and Fashion MNIST.
+- **Models for computer vision II**: CNN Medium-DA, VGG16-DA, mini Xception (Kaggle dataset), Segmentation (Oxford-IIIT).
+- **Models for sentiment analysis**: MLP sent. analysis (imdb), bag-of-words 2g, 2LSTM-wembd, Transf encoder (aclImdb).
 - **Model Training Tracking**: Monitor training progress with dynamic loss and accuracy diagrams.
 - **Data Visualization**: Analyze datasets and outputs using built-in graphing tools.
 - **Cross-Platform Compatibility**: Runs on both Windows and Linux systems.
--- dld.py
+++ dld.py
@@ -1,7 +1,15 @@
 # Entry point program
 import os
-import tkinter as tk
+import warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppresses TensorFlow INFO and WARNING messages
+warnings.filterwarnings('ignore')         # Suppresses all warnings
+
 import tensorflow as tf
+# Set the logger to display only errors and critical messages
+tf.get_logger().setLevel('ERROR')
+
+import re
+import tkinter as tk
 import src.datasetsFrame as ds
 import src.lossDiagFrame as ld
 import src.evaluateFrame as ev
@@ -12,8 +20,13 @@ import src.predictFrame as pr
 # Disable all GPUs
 #tf.config.set_visible_devices([], 'GPU')
 
-# Set the logger to display only errors and critical messages
-tf.get_logger().setLevel('ERROR')
+# If available, force GPU initialization by performing a dummy operation
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    with tf.device('/GPU:0'):             # Replace '/GPU:0' with specific GPU device ID if needed
+        dummy = tf.constant([1.0])  
+        tf.print("GPU initialized successfully.")
+    print()
 
 # Setup GUI 
 sw_version = os.getcwd()[-2:]
@@ -22,20 +35,36 @@ root.title(f"Deep Learning Dashboard v. {sw_version}")
 root.minsize(1200, 800)
 root.option_add("*Font", "Tahoma 11")   # Global font style and size for Windows10
 
+# Finding the OS type 
+if os.name == "nt":
+    os_type = "windows"
+if os.name == "posix":
+    os_type = "linux"
+    
 # TensorFlow and CUDA versions
-tf_version = f"TF version: {tf.__version__} without CUDA"
-if tf.test.is_built_with_cuda():        # Checking CUDA
-    cuda_version = tf.sysconfig.get_build_info()["cuda_version"]
-    tf_version = f"TF version: {tf.__version__} with CUDA: {cuda_version[-3:]}"
+version = f"TF version: {tf.__version__} without CUDA"
+if gpus:                                # Checking CUDA
+    if os_type == "linux":
+        cuda_ver_pattern = re.compile(r'cuda-([\d.]+)') 
+        cuda_version = cuda_ver_pattern.findall(os.environ.get("LD_LIBRARY_PATH"))
+        version = f"TF version: {tf.__version__} with CUDA: {cuda_version[0]}"
+    if os_type == "windows":
+        cuda_version = tf.sysconfig.get_build_info()["cuda_version"]    
+        version = f"TF version: {tf.__version__} with CUDA: {cuda_version[-3:]}"
 
-# Shared data and settings structures (e.g., a dictionary)
-shared_data = {}
+# Finding the TF version to set the keras model extension type
+if int(tf.__version__.split('.')[1]) <= 12:   # *.keras format available for TF > 2.12
+    ext = ".h5"
+else:
+    ext = ".keras"
 
 shared_settings = {
+    "datasets" : ["MNIST", "Fashion MNIST", "Kaggle", "Oxford-IIIT", "imdb", "aclImdb"],
     "models" : ["MLP-512", "MLP-2L-96", "MLP-128-Dropout", "CNN Small",
                 "CNN Medium-DA", "VGG16-DA", "mini Xception",
                 "Segmentation",
-                "MLP sent. analysis"],
+                "MLP sent. analysis",
+                "bag-of-words 2g", "2LSTM-wembd", "Transf Encoder"],
     "layers" : "",
     "layers_config" : {},
     "optimizer" : "",
@@ -44,10 +73,17 @@ shared_settings = {
     "epochs" : "",
     "batch_size" : "",
     "validation_split" : "",
+    "max_tokens" : 10000,
+    "seq_length" : 600,
     "status" : "idle",
     "duration" : "",
-    "version" : tf_version
+    "version" : version,
+    "os" : os_type,
+    "extension" : ext,
 }
+
+# Shared data and settings structures (e.g., a dictionary)
+shared_data = {}
 
 # Datasets frame
 dsFrame = ds.DatasetApp(root, shared_data, shared_settings)
@@ -56,10 +92,10 @@ dsFrame = ds.DatasetApp(root, shared_data, shared_settings)
 lossFrame = ld.LossDiagApp(root, shared_data, shared_settings)
 
 # Model evaluation frame
-evalFrame = ev.EvaluateApp(root, shared_data)
+evalFrame = ev.EvaluateApp(root, shared_data, shared_settings)
 
 # Model prediction frame
-predFrame = pr.PredictApp(root, shared_data)
+predFrame = pr.PredictApp(root, shared_data, shared_settings)
 
 root.protocol("Closing...", evalFrame.quit_app)
 root.mainloop()
